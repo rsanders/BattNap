8e66b7dcbb219bca2268ac75f3ca39fdcf3182b7
Robert Sanders <robert@esquimaux.org>
2010-09-29 09:03:20 -0400

    added status menu

--- AppDelegate.h
+++ AppDelegate.h
@@ -8,18 +8,37 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
+    NSWindow *sleepWindow;
+    NSWindow *aboutWindow;
+    
+    NSTextField *messageText;
+    NSStatusItem *statusItem;
+    NSMenu *statusMenu;
+
     BOOL isLowBatteryWarningAlertShowing;
     BOOL isSleepNotificationShowing;
-    NSWindow *sleepWindow;
-    NSTextField *messageText;
+    
+    BOOL paused;
 }
 
 @property (assign) IBOutlet NSWindow *window;
 @property (assign) IBOutlet NSWindow *sleepWindow;
+@property (assign) IBOutlet NSWindow *aboutWindow;
+
+@property (assign) IBOutlet NSMenu *statusMenu;
+
+
 
 - (void)showLowBatteryWarning;
 - (void)emergencySleep;
 - (IBAction)closeLowBatteryWarning:(id)sender;
+
 - (BOOL)isLowBatteryWarningShowing;
+
+// status menu items
+
+- (IBAction) menuTogglePause:(id)sender;
+- (IBAction) menuQuit:(id)sender;
+- (IBAction) menuAbout:(id)sender;
 
 @end
--- AppDelegate.m
+++ AppDelegate.m
@@ -14,12 +14,29 @@
 
 @synthesize window;
 @synthesize sleepWindow;
+@synthesize statusMenu;
+@synthesize aboutWindow;
+
+- (void)activateStatusMenu
+{
+    NSStatusBar *bar = [NSStatusBar systemStatusBar];
+    
+    statusItem = [bar statusItemWithLength:NSVariableStatusItemLength];
+    [statusItem retain];
+    
+    [statusItem setTitle: NSLocalizedString(@"Nap",@"")];
+    [statusItem setHighlightMode:YES];
+    [statusItem setMenu:statusMenu];
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     BatteryMonitor *monitor = [[BatteryMonitor alloc] init];
+    [self activateStatusMenu];
 }
 
 - (void)showLowBatteryWarning {
+    if (paused) return;
+
     if (![self isLowBatteryWarningShowing]) {
         [window setLevel:NSStatusWindowLevel];
         [window center];
@@ -34,18 +51,37 @@
     [window orderOut:nil];
 }
 
+
 - (BOOL)isLowBatteryWarningShowing
 {
     return isLowBatteryWarningAlertShowing;
 }
 
 - (void) emergencySleep {
+    if (paused) return;
+
     [sleepWindow setLevel:NSStatusWindowLevel];
     [sleepWindow center];
     isSleepNotificationShowing = YES;
     [sleepWindow makeKeyAndOrderFront:nil];
     
     system("/usr/bin/pmset sleepnow");    
+}
+
+#pragma mark Status Menu
+
+- (IBAction) menuTogglePause:(id)sender {
+    
+}
+
+- (IBAction) menuQuit:(id)sender {
+    exit(0);
+}
+
+- (IBAction) menuAbout:(id)sender {
+    [aboutWindow setLevel:NSStatusWindowLevel];
+    [aboutWindow center];
+    [aboutWindow makeKeyAndOrderFront:nil];
 }
 
 @end
